7a6b8f22dfd5e42a0a8df409081609b88f35e22d
leohsuzuki <leonardo.suzuki@hotmail.com>
2021-10-18 01:16:33 -0300

    feat: adiciona componentes autenticacao app e api

--- web-api/src/api/auth/services/auth.service.ts
+++ web-api/src/api/auth/services/auth.service.ts
@@ -1,17 +1,41 @@
 import {Injectable} from '@nestjs/common';
 import {IAuthRepository} from '../../../domain/auth/repositories/iauth.repository';
-import {LoginDto} from '../../../domain/auth/models/dtos/login.dto';
+import {AuthRepository} from '../../../infra/auth/repositories/auth.repository';
+import {LoginDto} from '../../../domain/auth/dtos/login.dto';
 import {LoginResult} from '../../../domain/auth/models/results/login.result';
-import {LoginCommand} from '../../../domain/auth/commands/login.command';
+import {RegisterDto} from '../../../domain/auth/dtos/register.dto';
+import {RegisterResult} from '../../../domain/auth/models/results/register.result';
+import {SendPasswordResetCodeDto} from '../../../domain/auth/dtos/send-password-reset-code.dto';
+import {SendPasswordResetCodeResult} from '../../../domain/auth/models/results/send-password-reset-code.result';
+import {ValidatePasswordResetCodeDto} from '../../../domain/auth/dtos/validate-password-reset-code.dto';
+import {ValidatePasswordResetCodeResult} from '../../../domain/auth/models/results/validate-password-reset-code.result';
+import {ChangePasswordDto} from '../../../domain/auth/dtos/change-password.dto';
+import {ChangePasswordResult} from '../../../domain/auth/models/results/change-password.result';
 
 @Injectable()
 export class AuthService implements IAuthRepository {
     constructor(
-        private readonly loginCommand: LoginCommand
+        private readonly repository: AuthRepository
     ) {
     }
 
-    login(input: LoginDto): Promise<LoginResult> {
-        return this.loginCommand.execute(input);
+    async login(input: LoginDto): Promise<LoginResult> {
+        return await this.repository.login(input);
+    }
+
+    async register(input: RegisterDto): Promise<RegisterResult> {
+        return await this.repository.register(input);
+    }
+
+    async sendPasswordResetCode(input: SendPasswordResetCodeDto): Promise<SendPasswordResetCodeResult> {
+        return await this.repository.sendPasswordResetCode(input);
+    }
+
+    async validatePasswordResetCode(input: ValidatePasswordResetCodeDto): Promise<ValidatePasswordResetCodeResult> {
+        return await this.repository.validatePasswordResetCode(input);
+    }
+
+    async changePassword(input: ChangePasswordDto): Promise<ChangePasswordResult> {
+        return await this.repository.changePassword(input);
     }
 }
