65e3b379c28dc4ba0608eef6e4fe87520dc0e2da
leohsuzuki <leonardo.suzuki@hotmail.com>
2021-12-20 02:39:15 -0300

    feat: adiciona layout confirmacao email

--- web-api/src/domain/auth/enums/register-message.enum.ts
+++ web-api/src/domain/auth/enums/register-message.enum.ts
@@ -1,5 +1,5 @@
 export enum RegisterMessage {
     EmailAddressAlreadyExists = 'Endereço de email já existe.',
     UsernameAlreadyExists = 'Nome de usuário já está em uso.',
-    SuccessfulRegistration = 'Cadastro bem sucedido, verifique sua caixa de endereço eletrônico para ativar sua conta.'
+    SuccessfulRegistration = 'Cadastro concluído, verifique a caixa de email para ativar sua conta.'
 }
